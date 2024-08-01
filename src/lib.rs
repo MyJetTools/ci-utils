eb24c45c35bdb0dad1dbc739115e1b6fe74ea0cc
amigin <amigin@gmail.com>
2024-08-01 20:14:35 +0300

    fixes with download_file

--- src/lib.rs
+++ src/lib.rs
@@ -15,6 +15,30 @@ pub fn build_proto_from_file(path: &str) {
     tonic_build::compile_protos(path).unwrap();
 }
 
+pub fn download_file(url_resource: &str, dest_path: &str) {
+    let response = reqwest::blocking::get(url_resource).unwrap();
+
+    if !response.status().is_success() {
+        panic!(
+            "Failed to download file {}. Http Status is: {}",
+            url_resource,
+            response.status()
+        );
+    }
+
+    let content = response.text().unwrap();
+
+    let mut f = std::fs::OpenOptions::new()
+        .write(true)
+        .truncate(true)
+        .create(true)
+        .open(dest_path)
+        .unwrap();
+
+    std::io::Write::write_all(&mut f, content.as_bytes()).unwrap();
+    std::io::Write::flush(&mut f).unwrap();
+}
+
 pub fn sync_and_build_proto_file_with_builder(
     url_resource: &str,
     proto_file_name: &str,
