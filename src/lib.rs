23874a3b55f38bc3ab4491bfba0aea536cfb71e3
amigin <amigin@gmail.com>
2024-08-22 19:14:34 +0300

    Added Ability to download private repo from github

--- src/lib.rs
+++ src/lib.rs
@@ -11,6 +11,18 @@ pub fn sync_and_build_proto_file(url_resource: &str, proto_file_name: &str) {
     println!("Proto file {} is compiled", proto_file_name);
 }
 
+pub fn sync_and_build_proto_file_from_private_github_repo(
+    repo_owner_name: &str,
+    repo_name: &str,
+    file_path: &str,
+) {
+    let proto_path_and_file =
+        prepare_proto_files_from_private_github(repo_owner_name, repo_name, file_path);
+
+    tonic_build::compile_protos(proto_path_and_file.as_str()).unwrap();
+    println!("Proto file {} is compiled", file_path);
+}
+
 pub fn build_proto_from_file(path: &str) {
     tonic_build::compile_protos(path).unwrap();
 }
@@ -105,6 +117,71 @@ fn prepare_proto_files(url_resource: &str, proto_file_name: &str) -> String {
         .unwrap();
 
     std::io::Write::write_all(&mut f, content.as_bytes()).unwrap();
+    std::io::Write::flush(&mut f).unwrap();
+    println!("Proto file {} is updated", proto_file_name);
+
+    proto_path_and_file
+}
+
+fn prepare_proto_files_from_private_github(
+    repo_owner: &str,
+    repo_name: &str,
+    file_path: &str,
+) -> String {
+    use rust_extensions::base64::FromBase64;
+
+    let url = format!("https://api.github.com/repos/{repo_owner}/{repo_name}/contents/{file_path}");
+
+    let response = if let Ok(git_hub_token) = std::env::var("GIT_HUB_TOKEN") {
+        let client = reqwest::blocking::Client::new();
+        client
+            .get(url)
+            .header("Accept", "application/vnd.github+json")
+            .header("User-Agent", "RustCiBuilder")
+            .header("Authorization", format!("Bearer {}", git_hub_token))
+            .header("X-GitHub-Api-Version", "2022-11-28")
+            .send()
+            .unwrap()
+    } else {
+        panic!("Please set GIT_HUB_TOKEN environment variable");
+    };
+
+    if !response.status().is_success() {
+        panic!(
+            "Failed to download proto file. Http Status is: {}",
+            response.status()
+        );
+    }
+
+    let content = response.text().unwrap();
+
+    let json_value = serde_json::from_str::<serde_json::Value>(content.as_str()).unwrap();
+
+    let map = json_value.as_object().unwrap();
+
+    let content = map.get("content").unwrap();
+
+    let mut proto_file_content = String::new();
+    for itm in content.as_str().unwrap().split("\n") {
+        proto_file_content.push_str(
+            String::from_utf8(itm.from_base64().unwrap())
+                .unwrap()
+                .as_str(),
+        );
+    }
+
+    let proto_file_name = file_path.split("/").last().unwrap();
+
+    let proto_path_and_file = format!("proto{}{}", std::path::MAIN_SEPARATOR, proto_file_name);
+
+    let mut f = std::fs::OpenOptions::new()
+        .write(true)
+        .truncate(true)
+        .create(true)
+        .open(proto_path_and_file.as_str())
+        .unwrap();
+
+    std::io::Write::write_all(&mut f, proto_file_content.as_bytes()).unwrap();
     std::io::Write::flush(&mut f).unwrap();
     println!("Proto file {} is updated", proto_file_name);
 
