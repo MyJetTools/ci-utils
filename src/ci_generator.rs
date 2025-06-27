0f16931d937df71caefb11955c09bd0804895743
amigin <amigin@gmail.com>
2025-06-27 12:40:33 +0300

    Fixes

--- src/ci_generator.rs
+++ src/ci_generator.rs
@@ -1,12 +1,12 @@
 #[derive(Clone, Copy)]
 pub enum DockerFileType {
-    Basic(&'static str),
+    Basic,
 }
 
 impl DockerFileType {
-    pub fn generate_docker_file(&self) {
+    pub fn generate_docker_file(&self, service_name: &'static str) {
         match self {
-            DockerFileType::Basic(service_name) => {
+            DockerFileType::Basic => {
                 let contents = format!("FROM ubuntu:22.04\nCOPY ./target/release/{service_name} ./target/release/{service_name}\nENTRYPOINT [\"./target/release/{service_name}\"]");
                 std::fs::write("Dockerfile", contents).unwrap();
             }
@@ -15,22 +15,26 @@ impl DockerFileType {
 }
 
 pub struct CiGenerator {
+    service_name: &'static str,
     docker_file: Option<DockerFileType>,
 }
 
 impl CiGenerator {
-    pub fn new() -> Self {
-        Self { docker_file: None }
+    pub fn new(service_name: &'static str) -> Self {
+        Self {
+            service_name,
+            docker_file: None,
+        }
     }
 
-    pub fn as_basic_service(mut self, service_name: &'static str) -> Self {
+    pub fn as_basic_service(mut self) -> Self {
         self.docker_file = Some(DockerFileType::Basic);
         self
     }
 
     pub fn build(self) {
         if let Some(docker_file) = self.docker_file {
-            docker_file.generate_docker_file();
+            docker_file.generate_docker_file(self.service_name);
         }
     }
 }
