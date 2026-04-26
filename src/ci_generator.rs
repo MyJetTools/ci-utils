48750a007443a96597ea5ee1a79271b3d7b19235
amigin <amigin@gmail.com>
2026-04-26 14:47:18 +0300

    Fixes

--- src/ci_generator.rs
+++ src/ci_generator.rs
@@ -5,7 +5,7 @@ use crate::ProtoFileBuilder;
 const CHECKOUT_VERSION: &str = "v6.0.2";
 const RUST_TOOLCHAIN_VERSION: &str = "v1.15.2";
 const DIOXUS_VERSION: &str = "0.7.6";
-const DIOXUS_DOCKER_IMAGE_DEFAULT: &str = "myjettools/dioxus-docker:0.7.6";
+const DIOXUS_DOCKER_IMAGE_DEFAULT: &str = "ghcr.io/myjettools/dioxus-docker:0.7.6";
 const DEFAULT_DOCKER_IMAGE_NAME: &str = "ghcr.io/${{ github.repository }}";
 
 #[derive(Clone, Copy)]
